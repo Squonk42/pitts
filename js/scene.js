8a0268da001188e6a56566339699d702b2ca26f1
Michel Stempin <michel.stempin@wanadoo.fr>
2016-09-15 00:05:30 +0200

    Added spitfiermkVb

--- js/scene.js
+++ js/scene.js
@@ -1,6 +1,7 @@
 var models = ['aircrane', 'antonov', 'bv170', 'canadair', 'clipper', 'dr400',
 	      'f16', 'f80', 'fouga', 'geebee', 'me262', 'p38', 'p51d', 'pitts',
-	      'rafale', 'spitfire', 'spitfireIIa', 'starfighter', 'zlin50lx'];
+	      'rafale', 'spitfire', 'spitfireIIa', 'spitfiremkVb', 'starfighter',
+	      'zlin50lx'];
 var name = null;
 var container, camera, scene, renderer, controls, clock, model = null;
 var proppeler = null;
